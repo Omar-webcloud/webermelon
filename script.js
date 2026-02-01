e61db3632963e9bfc6bf9d4ad68305363a55bf75
Md Omar Faruk Chowdhury <ofcjim16@gmail.com>
2026-02-01 23:19:51 +0600

    nav, banner fix

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
 const scrollContainer = document.querySelector(".horizontal-scroll");
-scrollContainer.addEventListener("wheel", (event) => {
+if (scrollContainer) {
+  scrollContainer.addEventListener("wheel", (event) => {
     event.preventDefault();
     scrollContainer.scrollLeft += event.deltaY;
+  });
+}
 
-});
+const toggler = document.querySelector(".nav-toggler");
+const sideMenu = document.querySelector(".side-menu");
+const closeBtn = document.querySelector(".close-btn");
+const overlay = document.querySelector(".menu-overlay");
 
+function toggleMenu() {
+  sideMenu.classList.toggle("active");
+  overlay.classList.toggle("active");
+}
 
-
+if (toggler) {
+  toggler.addEventListener("click", toggleMenu);
+}
+if (closeBtn) {
+  closeBtn.addEventListener("click", toggleMenu);
+}
+if (overlay) {
+  overlay.addEventListener("click", toggleMenu);
+}
