2f10e49489a4b5c638743c4461049f5ffd517724
Md Omar Faruk Chowdhury <ofcjim16@gmail.com>
2026-02-02 16:14:40 +0600

    logo fix

--- script.js
+++ script.js
@@ -37,3 +37,64 @@ tabs.forEach((tab) => {
     tab.classList.add("active");
   });
 });
+
+
+const productData = [
+  {
+    id: 1,
+    title: "Vintage Single Sofa 2024",
+    category: "Sofa",
+    price: 18500,
+    image: "assets/sofa_1.png"
+  },
+  {
+    id: 2,
+    title: "Modern Chair Classic",
+    category: "Chair",
+    price: 18000,
+    image: "assets/chair_1.png"
+  },
+  {
+    id: 3,
+    title: "Comfort Wooden Chair",
+    category: "Chair",
+    price: 18000,
+    image: "assets/chair_2.png"
+  },
+  {
+    id: 4,
+    title: "Luxury Double Sofa",
+    category: "Sofa",
+    price: 18000,
+    image: "assets/sofa_1.png"
+  },
+  {
+    id: 5,
+    title: "Premium Sofa Set",
+    category: "Sofa",
+    price: 18500,
+    image: "assets/sofa_1.png"
+  },
+  {
+    id: 6,
+    title: "Classic Chair 2024",
+    category: "Chair",
+    price: 18000,
+    image: "assets/chair_1.png"
+  },
+  {
+    id: 7,
+    title: "Minimal Chair",
+    category: "Chair",
+    price: 18000,
+    image: "assets/chair_2.png"
+  },
+  {
+    id: 8,
+    title: "Elegant Sofa",
+    category: "Sofa",
+    price: 18000,
+    image: "assets/sofa_1.png"
+  }
+];
+
